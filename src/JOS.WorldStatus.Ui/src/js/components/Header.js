d1dce3d55d2a5313e5163ab8d2911654a76d91f9
Josef Ottosson <ottossonjosef@gmail.com>
2017-04-01 22:05:00 +0200

    Adds eslint and fixes some webpack2 stuff

--- src/JOS.WorldStatus.Ui/src/js/components/Header.js
+++ src/JOS.WorldStatus.Ui/src/js/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class Header extends React.Component {
   render() {
     return (
-      <header>Its working wopp wopp! :D </header>
+      <header>Its working wopp wopp! :D :D :D </header>
     );
   }
 }
